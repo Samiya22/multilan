479ef72a2f694089c23206d127649c665a2df889
samiya22 <samiya.sultanova21@gmail.com>
2022-05-04 10:29:22 +0500

    multilang start

---
+++ lanapp/models.py
@@ -0,0 +1,13 @@
+from cgitb import text
+from django.db import models
+
+# Create your models here.
+
+
+class Malumot(models.Model):
+    nomi = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.nomi
