bc240c4c962314ed22ab44f3a3ef57d171d9eaad
samiya22 <samiya.sultanova21@gmail.com>
2022-05-09 16:39:43 +0500

    locale fayl qo'shildi

--- lanapp/admin.py
+++ lanapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Malumot
+from modeltranslation.admin import TranslationAdmin
 
 # Register your models here.
 @admin.register(Malumot)
-class MalumotAdmin(admin.ModelAdmin):
+class MalumotAdmin(TranslationAdmin):
     prepopulated_fields = {'slug': ('nomi',)}
 
